eca23cbec1d0c1e97160e1aaa6f60f9c17f8225e
dimit <dimitriaigle@gmail>
2022-09-26 20:02:33 -0400

    partie une tp finish

--- Tp3/main.py
+++ Tp3/main.py
@@ -1,7 +1,7 @@
 import csv
 import pandas
-df = pandas.read_csv("PSRA.csv")
-with open('PSRA.csv', newline='') as csvfile:
+df = pandas.read_csv("PRSA.csv")
+with open('PRSA.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     trou  = False
     nbVal = 0
@@ -16,19 +16,16 @@ with open('PSRA.csv', newline='') as csvfile:
                 else :
                     if (nbVal > 0):
                         NbValManquante.append(nbVal)
-                        #print("Nombre de valeurs manquantes : " , nbVal)
                         if (nbVal < 15):
                             PrevTrou = int(row['No']) - int(nbVal+1)
                             PrevTrouData = df.loc[(df['No'] == PrevTrou)]
                             ValPrev = int(PrevTrouData['pm2.5'])
                             ValSuiv = int(row['pm2.5'])
-                            #print("Début du trou "+ str(PrevTrou+1) + ": \n" +"Précédent : " + str(PrevTrou) +
-                                  #" valeur : "+ str(ValPrev)+ "\n" + "Suivant : " + row['No'] + " valeur : "+ str(ValSuiv))
                             i = abs(ValSuiv-ValPrev)/nbVal+1
                             valI.append(i)
-                            #print("valeur de i : " + str(i))
                         else :
                             valI.append(0)
+
                     nbVal=0
                 if (row['pm2.5'] == 'NA' and trou == False):
                     DebTrou.append(int(row['No']))
@@ -37,7 +34,7 @@ with open('PSRA.csv', newline='') as csvfile:
                     a = int(row['No'])-1
                     FinTrou.append(a)
                     trou = False
-    print(" Trou  | Debut | Fin | ValM | Coeff")
+    print(" Trou  | Debut | Fin | ValM | Incrément")
     for i in range(len(valI)) :
         if (i < 9):
             print(" ",i+1,"   |",DebTrou[i]," |",FinTrou[i]," |",NbValManquante[i]," |",valI[i])
@@ -45,3 +42,27 @@ with open('PSRA.csv', newline='') as csvfile:
             print(" ", i + 1, "  |", DebTrou[i], " |", FinTrou[i], " |", NbValManquante[i], " |", valI[i])
         else :
             print(" ", i + 1, "|", DebTrou[i], " |", FinTrou[i], " |", NbValManquante[i], " |", valI[i])
+    for inc in range(len(valI)) :
+        if (NbValManquante[inc]>15):
+            testeur = 0
+            for j in range(NbValManquante[inc]):
+                df.loc[df["No"] == DebTrou[inc]+j , "pm2.5"] = "?"
+        else:
+            for j in range(NbValManquante[inc]):
+                #print(valI[inc]*j+1)
+                df.loc[df["No"] == DebTrou[inc] + j, "pm2.5"] = valI[inc]*j+1
+    df.to_csv("tp2_1.csv", index=False)
+    print("Save on file tp2_1.csv")
+    nbNA = 0
+    with open('tp2_1.csv', newline='') as tpfile:
+        tpReader = csv.DictReader(tpfile)
+        for row in tpReader:
+            print(nbNA)
+            if (row['pm2.5'] == 'NA'):
+                nbNA += 1
+
+
+    if(nbNA == 0):
+        print("plus aucune trou de plus de 15 valeurs")
+    else:
+        print("il existe des trou toujours")
