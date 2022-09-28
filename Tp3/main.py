1a9c1e52692c1266457f776c6d3eb38089bb406e
dimit <dimitriaigle@gmail>
2022-09-28 10:17:01 -0400

    tp3 2b

--- Tp3/main.py
+++ Tp3/main.py
@@ -10,7 +10,14 @@ with open('PRSA.csv', newline='') as csvfile:
     valI = []
     DebTrou = []
     FinTrou = []
+    valcbwd = []
     for row in reader :
+                test = False
+                for val in range(len(valcbwd)):
+                    if(valcbwd[val] == row['cbwd']):
+                        test = True
+                if(test == False):
+                    valcbwd.append(row['cbwd'])
                 if(row['pm2.5'] == 'NA'):
                     nbVal+=1
                 else :
@@ -21,7 +28,7 @@ with open('PRSA.csv', newline='') as csvfile:
                             PrevTrouData = df.loc[(df['No'] == PrevTrou)]
                             ValPrev = int(PrevTrouData['pm2.5'])
                             ValSuiv = int(row['pm2.5'])
-                            i = abs(ValSuiv-ValPrev)/nbVal+1
+                            i = (ValSuiv-ValPrev)/(nbVal+1)
                             valI.append(i)
                         else :
                             valI.append(0)
@@ -34,7 +41,7 @@ with open('PRSA.csv', newline='') as csvfile:
                     a = int(row['No'])-1
                     FinTrou.append(a)
                     trou = False
-    f = open('DonnéePRSA.csv', 'w')
+    f = open('AIGLE_DonnéePRSA.csv', 'w')
     entetes = ["Trou", "Debut", "Fin", "ValManq", "Incré"]
     ligneEntete = ",".join(entetes) + "\n"
     f.write(ligneEntete)
@@ -57,19 +64,18 @@ with open('PRSA.csv', newline='') as csvfile:
                 df.loc[df["No"] == DebTrou[inc]+j , "pm2.5"] = "?"
         else:
             for j in range(NbValManquante[inc]):
-                #print(valI[inc]*j+1)
-                df.loc[df["No"] == DebTrou[inc] + j, "pm2.5"] = valI[inc]*j+1
-    df.to_csv("tp3_1.csv", index=False)
-    print("Save on file tp2_1.csv")
+                b = df.loc[df["No"] == DebTrou[inc] - 1]
+                df.loc[df["No"] == DebTrou[inc] + j, "pm2.5"] = float(b["pm2.5"]+valI[inc]*(j+1))
+    df.to_csv("AIGLE_tp3_1.csv", index=False)
+    print("Save on file AIGLE_tp2_1.csv")
     nbNA = 0
-    with open('tp3_1.csv', newline='') as tpfile:
+    with open('AIGLE_tp3_1.csv', newline='') as tpfile:
         tpReader = csv.DictReader(tpfile)
         for row in tpReader:
             if (row['pm2.5'] == 'NA'):
                 nbNA += 1
-
-
     if(nbNA == 0):
         print("plus aucune trou de plus de 15 valeurs")
     else:
         print("il existe des trou toujours")
+    print(valcbwd)
---
+++ Tp3/tp3_main2.py
@@ -0,0 +1,41 @@
+import csv
+import pandas
+df = pandas.read_csv("AIGLE_tp3_1.csv")
+f = open('AIGLE_tp2_2b.csv', 'w')
+entetes = ["pm2.5", "pm2.5_3", "pm2.5_4", "pm2.5_5"]
+ligneEntete = ",".join(entetes) + "\n"
+f.write(ligneEntete)
+with open('AIGLE_tp3_1.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+            if(int(row["No"])>30 ):
+                pm_3 = df.loc[df["No"] == (int(row["No"]) - 3)]
+                pm_4 = df.loc[df["No"] == (int(row["No"]) - 4)]
+                pm_5 = df.loc[df["No"] == (int(row["No"]) - 5)]
+                ligne = ""
+                if ("?" in str(row["pm2.5"])):
+                    ligne += "?,"
+                else:
+                    ligne += str(float(row["pm2.5"])) + ","
+
+                if("?" in str(pm_3["pm2.5"])):
+                    ligne += "?,"
+                else :
+                    ligne += str(float(pm_3["pm2.5"])) + ","
+
+                if("?" in str(pm_4["pm2.5"])):
+                    ligne += "?,"
+                else :
+                    ligne += str(float(pm_4["pm2.5"])) + ","
+
+                if("?" in str(pm_5["pm2.5"])):
+                    ligne += "?,"
+                else :
+                    ligne += str(float(pm_5["pm2.5"])) + ","
+                ligne += "\n"
+                f.write(ligne)
+
+f.close()
+
+
+
