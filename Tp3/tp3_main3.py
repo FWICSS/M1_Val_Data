8ed99249b38a2caa4896a6bf4b053ed91ba56226
dimit <dimitriaigle@gmail>
2022-09-29 10:12:17 -0400

    tp3 2c

---
+++ Tp3/tp3_main3.py
@@ -0,0 +1,19 @@
+import pandas
+df = pandas.read_csv("AIGLE_tp3_1.csv")
+
+df["pm2.5_3"] = ['?'] * len(df)
+df["pm2.5_4"] = ['?'] * len(df)
+df["pm2.5_5"] = ['?'] * len(df)
+
+
+for i, ligne in  enumerate(df.iterrows()):
+        if(int(df["No"][i])>29):
+                df["pm2.5_3"][i] = df["pm2.5"][i-3]
+                df["pm2.5_4"][i] = df["pm2.5"][i-4]
+                df["pm2.5_5"][i] = df["pm2.5"][i-5]
+df.drop(range(25,29))
+
+df.to_csv("AIGLE_tp3_2c.csv", index=False)
+
+
+
